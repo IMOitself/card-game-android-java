1857195e9890a0a3a01f48c3d11ab75206439a36
IMOitself <110971169+IMOitself@users.noreply.github.com>
2023-04-17 16:19:54 +0800

    Update on Animations.java
    prior to MainActivity.java, card animation is now transffered to this class. Some quality of life changes too

--- app/src/main/java/imo/card/gametest/Animations.java
+++ app/src/main/java/imo/card/gametest/Animations.java
@@ -3,10 +3,12 @@ package imo.card.gametest;
 import android.graphics.Color;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Button;
 
 public class Animations
 {
-	public static int duration = 500;
+	private static int duration = 500;
+	
 	public static void redTextAnim(final TextView textview){
 		textview.setTextColor(Color.parseColor("#FB1600"));
 		textview.animate().scaleX(1.1f).scaleY(1.1f).setDuration(duration)
@@ -39,6 +41,28 @@ public class Animations
 			.withEndAction(new Runnable() { @Override public void run() {
 					view.setScaleX(1);
 					view.setScaleY(1);
+				} })
+			.start();
+	}
+	public static void cardAnim(final Button btn1, final Button btn2, final View cardLayout, final View cardBackLayout){
+		btn1.setEnabled(false);
+		btn2.setEnabled(false);
+		cardLayout.setVisibility(View.GONE);
+		cardBackLayout.setTranslationY(50);
+		cardBackLayout.setVisibility(View.VISIBLE);
+
+		cardBackLayout.animate().translationY(0).setDuration(duration)
+			.withEndAction(new Runnable() { @Override public void run() {
+					cardBackLayout.animate().scaleX(0).setDuration(duration).withEndAction(new Runnable() { @Override public void run() {
+								cardBackLayout.setVisibility(View.GONE);
+								cardBackLayout.setScaleX(1);
+								cardLayout.setScaleX(0);
+								cardLayout.setVisibility(View.VISIBLE);
+								cardLayout.animate().scaleX(1).setDuration(duration).start();
+								btn1.setEnabled(true);
+								btn2.setEnabled(true);
+							} })
+						.start();	
 				} })
 			.start();
 	}
