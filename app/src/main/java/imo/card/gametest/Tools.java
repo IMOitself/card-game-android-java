b9518625e65a6e405d8e19eb8386fa4ac2b310db
IMOitself <110971169+IMOitself@users.noreply.github.com>
2023-04-21 11:12:08 +0800

    Update on Tools.java
    changes setRoundedViewWithStroke to a much better name and also added stroke dash and stroke dash gap

--- app/src/main/java/imo/card/gametest/Tools.java
+++ app/src/main/java/imo/card/gametest/Tools.java
@@ -1,8 +1,12 @@
 package imo.card.gametest;
 
-import android.graphics.drawable.GradientDrawable;
+import android.app.Activity;
+import android.widget.TextView;
+import android.view.ViewGroup;
 import android.view.View;
-
+import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.ColorDrawable;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,31 +17,36 @@ public class Tools
 		if (width != 0) view.getLayoutParams().width = width;
 		if (height != 0) view.getLayoutParams().height = height;
 	}
-	public static void setRoundedViewWithStroke(View layout, int backgroundColor, int strokeColor, int strokeWidth, int cornerRadius, float opacity) {
+	public static void setCustomBgWithStroke(View view, int bgColor, int cornerRadius, int strokeWidth, int strokeColor, float strokeAlpha, float dashWidth, float dashGap) {
 		GradientDrawable shape = new GradientDrawable();
 		shape.setShape(GradientDrawable.RECTANGLE);
-		shape.setColor(backgroundColor);
+		shape.setColor(bgColor);
 		shape.setCornerRadius(cornerRadius);
-		shape.setStroke(strokeWidth, strokeColor & 0xFFFFFF | ((int)(opacity * 255)) << 24);
-		layout.setBackgroundDrawable(shape);//display the shape as background of the view
+		int strokeColorWithAlpha = strokeColor & 0xFFFFFF | ((int)(strokeAlpha * 255)) << 24;
+		if(dashWidth != 0 || dashGap != 0){
+			// Set the stroke with dash
+			shape.setStroke(strokeWidth, strokeColorWithAlpha, dashWidth, dashGap);
+		}else{
+			//Set the stroke without dash
+			shape.setStroke(strokeWidth, strokeColorWithAlpha);
+		}
+		view.setBackground(shape);
 	}
-	
 	public static void importDataToArraylist(List<Map<String, String>> arraylist,
 											 String textFile,
 											 String splitItemsBy,
 											 String splitContentsBy){
-		String[] rawItems = textFile.split(splitItemsBy);//separate the contents to individual sections
+		String[] rawItems = textFile.split(splitItemsBy);//seperate the contents to individual sections
 		for (String rawItem : rawItems) {//loop to every sections made
-			String rawContent = rawItem.replaceFirst(splitContentsBy, "");//remove the leading desired char before separating
-			String[] keyValues = rawContent.split(splitContentsBy);//separate into pieces by the desired char
+			String rawContent = rawItem.replaceFirst(splitContentsBy, "");//remove the leading desired char before seperating
+			String[] keyValues = rawContent.split(splitContentsBy);//seperate into pieces by the desired char
 			Map<String, String> newMap = new HashMap<>();
-			for (String keyValue : keyValues) {//get the separated pieces
+			for (String keyValue : keyValues) {//get the seperated pieces
 				if(keyValue.contains("=")){
-					String[] splitKeyValue = keyValue.split("=");//separate into 2 pieces by =
+					String[] splitKeyValue = keyValue.split("=");//seperate into 2 pieces by =
 					newMap.put(splitKeyValue[0].trim(), splitKeyValue[1].trim());//put the 1st piece into the key and 2nd piece to the value of a map
 				}
 			}
-			System.out.println("uwu");
 			arraylist.add(newMap);//add the maps
 		}
 		System.out.println(arraylist);
